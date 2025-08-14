48882333aa37d4f89734cb7a8eefdaf30993730c
NatalieWeaver <natalieweaver720@gmail.com>
2025-08-13 20:25:01 -0700

    workshop changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,23 @@ import plotly.express as px
 
 df = pd.read_csv("state_data.csv")
 
-st.header("US State Demographics")
+st.header("Changes in US State Demographics")
 
 # Let user select which state to graph
 state = st.selectbox("State:", df["State"].unique())
+demographic = st.selectbox("Demographic:", ["Total Population", "Median Household Income"])
 
-# Create a graph of total population
-df_state = df[df["State"] == state]
-fig = px.line(
-    df_state, x="Year", y="Total Population", title=f"Total Population of {state}"
-)
-st.plotly_chart(fig)
+tab1, tab2 = st.tabs(["Plot", "Table"])
+
+# Create a graph of demographic
+with tab1:
+    df_state = df[df["State"] == state]
+    fig = px.line(
+        df_state, x="Year", y=demographic, title=f"{demographic} of {state}"
+    )
+    st.plotly_chart(fig)
 
 # Show the entire dataframe
-st.write("All Data")
-st.dataframe(df)
+with tab2:
+    st.write(f"{state} Data")
+    st.dataframe(df_state)
